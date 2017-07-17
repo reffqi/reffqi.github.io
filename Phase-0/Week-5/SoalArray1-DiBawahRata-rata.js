a4d9cdd1fbe6f33c4ff3c82356504b839eebf4b3
Rifqi Anugrah <rifqi@localhost.localdomain>
2017-07-17 13:27:04 +0700

    edited

---
+++ Phase-0/Week-5/SoalArray1-DiBawahRata-rata.js
@@ -0,0 +1,25 @@
+/*
+HACKTIV8 Practice
+
+Nama:
+
+==========
+Soal Array 1 - Di Bawah Rata-rata
+==========
+
+[INSTRUKSI]
+Tersedia 1 input Array
+Output: array berisi semua angka dalam array input yang di bawah rata-rata
+
+[CONTOH]
+1) [1, 2, 3, 4, 5]  --> [1, 2]
+*/
+
+function belowAverage(arr) {
+    // start code here
+}
+
+console.log(belowAverage([1, 2, 3, 4, 5])); // [1, 2]
+console.log(belowAverage([4, 2, 5, 1]));    // [2, 1]
+console.log(belowAverage([5, 5, 5]));       // []
+console.log(belowAverage([10, 2, 3, 2]));   // [2, 3, 2]
--- Phase-0/Week-5/SoalArray2-Duplikat.js
+++ Phase-0/Week-5/SoalArray2-Duplikat.js
@@ -1,7 +1,7 @@
 /*
 HACKTIV8 Practice
 
-Nama : Rifqi Anugrah
+Nama:
 
 ==========
 Soal Array 2 - Duplikat
@@ -17,7 +17,7 @@ Output: array yang telah di-sorted, semua duplikat dihapus
 
 function hapusDuplikat(arr) {
     // start code here
-    var angka = arr.sort(function(a, b){return a - b;});
+    var angka = arr.sort();
     return Array.from(new Set(angka));
 }
 
@@ -25,4 +25,3 @@ console.log(hapusDuplikat([1, 4, 3, 1, 2, 2, 4]));    // [1, 2, 3, 4]
 console.log(hapusDuplikat([2, 2, 2, 2]));            // [2]
 console.log(hapusDuplikat([1, 2, 3]));                // [1, 2, 3]
 console.log(hapusDuplikat([6, 5, 3, 2]));            // [2, 3, 5, 6]
-
